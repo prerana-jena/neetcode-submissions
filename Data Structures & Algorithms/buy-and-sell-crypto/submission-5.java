fac42a70ca70b132b0418dbd485c4b92482171e4
prerana-jena <159808146+prerana-jena@users.noreply.github.com>
2026-05-24 15:27:09 +0000

    Add: buy-and-sell-crypto - submission-5

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-5.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min=Integer.MAX_VALUE;
+        int max=0;
+        for(int i=0;i<prices.length;i++)
+        {
+            if(prices[i]<min)
+            {
+                min=prices[i];
+            }
+            int profit=prices[i]-min;
+            max=Math.max(profit,max);
+        }
+        return max;
+    }
+}
